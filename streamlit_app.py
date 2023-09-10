74c09041938bde17f960d2c81cc4d144025d944e
Sayali-Amrute <89139222+Sayali-Amrute@users.noreply.github.com>
2023-09-10 16:35:49 +0530

    Update streamlit_app.py
    fetched data from s3 bucket and displayed it on the app using pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+
 st.title("My Mom's New Healthy Diner!")
 st.header("Breakfast Favorites")
 st.text("🥣 Omega 3 & Blueberry Oatmeal")
@@ -7,3 +9,5 @@ st.text("🐔 Hard Boiled Free-Range Egg")
 st.text("🥑🍞 Avocado Toast")
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruits_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruits_list)
