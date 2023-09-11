47a4ee4033d8c74c6a2c8d908f9514947aba2382
Sayali-Amrute <89139222+Sayali-Amrute@users.noreply.github.com>
2023-09-11 23:05:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-
+import requests
 st.title("My Mom's New Healthy Diner!")
 st.header("Breakfast Favorites")
 st.text("🥣 Omega 3 & Blueberry Oatmeal")
@@ -17,6 +17,6 @@ st.dataframe(fruits_to_show)
 
 
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
