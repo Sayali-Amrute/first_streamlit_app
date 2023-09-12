a2ecfe00800b8e82445dae63624cf763c3a0ddbe
Sayali-Amrute <89139222+Sayali-Amrute@users.noreply.github.com>
2023-09-12 11:52:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,30 @@
-import streamlit as st
+import streamlit
 import pandas as pd
 
-st.title("My Mom's New Healthy Diner!")
-st.header("Breakfast Favorites")
-st.text("🥣 Omega 3 & Blueberry Oatmeal")
-st.text("🥗 Kale, Spinach & Rocket Smoothie")
-st.text("🐔 Hard Boiled Free-Range Egg")
-st.text("🥑🍞 Avocado Toast")
+streamlit.title("My Mom's New Healthy Diner!")
+streamlit.header("Breakfast Favorites")
+streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard Boiled Free-Range Egg")
+streamlit.text("🥑🍞 Avocado Toast")
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruits_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruits_list = my_fruits_list.set_index('Fruit')
-fruits_selected = st.multiselect("Pick some fruits:", list(my_fruits_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruits_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruits_list.loc[fruits_selected]
-st.dataframe(fruits_to_show)
-st.header("Fruityvice Fruit Advice!")
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ', fruit_choice)
+streamlit.dataframe(fruits_to_show)
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response)
+#streamlit.text(fruityvice_response)
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-st.dataframe(fruityvice_normalized)
+streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
